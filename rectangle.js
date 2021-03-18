2590761be7c2c0e4d0c3a00613d66680cf7841a4
coder girl <56164343+Susan-Wangari@users.noreply.github.com>
2021-03-18 06:49:42 -0700

    creates an object(rectangle) then gets the area and perimeter

---
+++ rectangle.js
@@ -0,0 +1,8 @@
+ */
+function Rectangle(a, b) {
+    //create an object and assign values to it
+    this.length = a;
+    this.width = b;
+    this.perimeter = (a + b) * 2;
+    this.area = a * b;
+}

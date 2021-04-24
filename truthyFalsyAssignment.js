796cc48aa8f47422fc60ffc6d7e9b86c4664b70a
coder girl <56164343+Susan-Wangari@users.noreply.github.com>
2021-04-24 11:01:57 -0700

    Truthy and falsy assignment

---
+++ truthyFalsyAssignment.js
@@ -0,0 +1,6 @@
+let tool = ''; //a falsy
+
+// Using short circuit evaluation to assign  writingUtensil variable 
+let writingUtensil = tool || 'pen';
+
+console.log(`The ${writingUtensil} is mightier than the sword.`);

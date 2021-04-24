56030a56b2528eee03020f7a05bf6293a1f1952c
coder girl <56164343+Susan-Wangari@users.noreply.github.com>
2021-04-24 12:38:04 -0700

    ternary operator

---
+++ ternaryOperator.js
@@ -0,0 +1,12 @@
+//2 statements written differently but uing the same operator
+let isCorrect = true;
+
+isCorrect ? console.log('Correct!') :console.log('Incorrect!');
+
+
+let favoritePhrase = 'Love That!';
+
+favoritePhrase === 'Love That!' ? 
+  console.log('I love that!')
+:
+  console.log("I don't love that!");

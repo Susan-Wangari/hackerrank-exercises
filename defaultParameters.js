95f45ddf3d9384622076f892569de63994008e7b
coder girl <56164343+Susan-Wangari@users.noreply.github.com>
2021-04-28 02:36:08 -0700

    default parameters in JavaScript

---
+++ defaultParameters.js
@@ -0,0 +1,6 @@
+//items 1, 2 and 3 are given default parameters. These are passed to the console if no argument is passed
+function makeShoppingList(item1 = 'milk', item2 = 'bread', item3 = 'eggs'){
+  console.log(`Remember to buy ${item1}`);
+  console.log(`Remember to buy ${item2}`);
+  console.log(`Remember to buy ${item3}`);
+}

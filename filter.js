a89ba2c9d3a5923c400cb68fdc1ca05f2c5f265a
coder girl <56164343+Susan-Wangari@users.noreply.github.com>
2021-08-06 09:50:03 +0300

    using filter method  to modify and return a new array

---
+++ filter.js
@@ -0,0 +1,18 @@
+const randomNumbers = [375, 200, 3.14, 7, 13, 852];
+
+// Call .filter() on randomNumbers below
+
+
+const favoriteWords = ['nostalgia', 'hyperbole', 'fervent', 'esoteric', 'serene'];
+
+
+// Call .filter() on favoriteWords below
+
+const smallNumbers = randomNumbers.filter(num => {
+return num < 250 });
+
+const longFavoriteWords = favoriteWords.filter(word => {
+  return word.length > 7
+});
+
+
